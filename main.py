9e80467eeac1f0f8e94e71a2705918d21078aeea
sugi-01096 <nodoguro04@gmail.com>
2023-08-25 20:27:41 +0900

    xz

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+
+def main():
+    st.title('チームごとの順位とスコアの表示')
+
+    teams = []
+    scores1 = []
+    scores2 = []
+    while True:
+        team_name = st.text_input("チーム名を入力してください")
+        score1 = st.number_input("スコア1を入力してください", min_value=0, value=0, step=1)
+        score2 = st.number_input("スコア2を入力してください", min_value=0, value=0, step=1)
+        
+        if st.button("チームを追加"):
+            teams.append(team_name)
+            scores1.append(score1)
+            scores2.append(score2)
+        
+        if st.button("終了"):
+            break
+    
+    if teams:
+        df = pd.DataFrame({'Team': teams, 'Score1': scores1, 'Score2': scores2})
+        
+        # 各スコアごとに順位を付与
+        df['Rank1'] = df['Score1'].rank(ascending=False).astype(int)
+        df['Rank2'] = df['Score2'].rank(ascending=False).astype(int)
+        
+        # スコア1とスコア2の合計を計算し、合計に基づいて順位を付与
+        df['TotalScore'] = df['Score1'] + df['Score2']
+        df['TotalRank'] = df['TotalScore'].rank(ascending=False).astype(int)
+        
+        st.write('チーム別の順位とスコア:')
+        st.table(df[['Team', 'Score1', 'Rank1', 'Score2', 'Rank2', 'TotalScore', 'TotalRank']].reset_index(drop=True))
+    else:
+        st.write("チームが追加されていません。")
+
+if __name__ == "__main__":
+    main()
