2e3ac8b649302a1a0639faea5550dbb548a6a64f
sugi-01096 <nodoguro04@gmail.com>
2023-08-28 03:22:27 +0900

    a

--- main.py
+++ main.py
@@ -7,24 +7,26 @@ def main():
     teams = []
     scores1 = []
     scores2 = []
-    key_counter = 0  # カウンタ変数を初期化
+    key_counter = 1  # カウンタ変数を初期化
 
     while True:
         team_name = st.text_input("チーム名を入力してください", key=f"team_name_input_{key_counter}")
         if not team_name:
-            break
+            break  # チーム名が空ならループを終了
         
-        score1 = st.number_input("スコア1を入力してください", min_value=0, value=0, step=10, key=f"score1_input_{key_counter}")
-        score2 = st.number_input("スコア2を入力してください", min_value=0, value=0, step=10, key=f"score2_input_{key_counter}")
+        score1 = st.number_input("スコア1を入力してください", min_value=0, value=0, step=1, key=f"score1_input_{key_counter}")
+        score2 = st.number_input("スコア2を入力してください", min_value=0, value=0, step=1, key=f"score2_input_{key_counter}")
         
         if st.button("チームを追加", key=f"add_button_{key_counter}"):
             teams.append(team_name)
             scores1.append(score1)
             scores2.append(score2)
-            
-            key_counter += 1  # カウンタ変数を増やす
+
+        
+        key_counter += 1  # カウンタ変数を増やす
     
     if teams:
+        df = pd.DataFrame({'Team': teams, 'Score1': scores1, 'Score2': scores2})
         df = pd.DataFrame({'Team': teams, 'Score1': scores1, 'Score2': scores2})
         
         # 各スコアごとに順位を付与
@@ -38,7 +40,7 @@ def main():
         st.write('チーム別の順位とスコア:')
         st.table(df[['Team', 'Score1', 'Rank1', 'Score2', 'Rank2', 'TotalScore', 'TotalRank']].reset_index(drop=True))
     else:
-        st.write("チームが追加されていません。")
+        st.write("チームが追加されていません。")# 以降のコードは同様
 
 if __name__ == "__main__":
     main()
