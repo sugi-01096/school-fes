b75f9fb29750fecc11a165036165b1ebcf9037d5
sugi-01096 <nodoguro04@gmail.com>
2023-08-28 03:45:30 +0900

    as

--- main.py
+++ main.py
@@ -4,28 +4,27 @@ import pandas as pd
 def main():
     st.title('チームごとの順位とスコアの表示')
 
-    teams = []
-    scores1 = []
-    scores2 = []
+    team_counter = 1
+    team_data = []
 
     while True:
-        team_name = st.text_input("チーム名を入力してください")
+        team_name = st.text_input("チーム名を入力してください", key=f"team_name_input_{team_counter}")
         if not team_name:
-            break
+            break  # チーム名が空ならループを終了
         
-        score1 = st.number_input("スコア1を入力してください", min_value=0, value=0, step=1)
-        score2 = st.number_input("スコア2を入力してください", min_value=0, value=0, step=1)
+        score1 = st.number_input("スコア1を入力してください", min_value=0, value=0, step=1, key=f"score1_input_{team_counter}")
+        score2 = st.number_input("スコア2を入力してください", min_value=0, value=0, step=1, key=f"score2_input_{team_counter}")
         
-        teams.append(team_name)
-        scores1.append(score1)
-        scores2.append(score2)
+        if st.button("チームを追加", key=f"add_button_{team_counter}"):
+            team_data.append({'Team': team_name, 'Score1': score1, 'Score2': score2})
+        
+        team_counter += 1
     
-    if teams:
-        df = pd.DataFrame({'Team': teams, 'Score1': scores1, 'Score2': scores2})
+    if team_data:
+        df = pd.DataFrame(team_data)
         
         df['Rank1'] = df['Score1'].rank(ascending=False).astype(int)
         df['Rank2'] = df['Score2'].rank(ascending=False).astype(int)
-        
         df['TotalScore'] = df['Score1'] + df['Score2']
         df['TotalRank'] = df['TotalScore'].rank(ascending=False).astype(int)
         
@@ -36,3 +35,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
